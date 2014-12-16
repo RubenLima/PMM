fe01b84393a80033c5794f8a9053c27159b9a34b
RubenLima <rubenperezperez304@gmail.com>
2014-12-16 11:28:00 +0100

    16-12

--- Solomibici/app/src/main/java/com/example/mati/solomibici/Juego.java
+++ Solomibici/app/src/main/java/com/example/mati/solomibici/Juego.java
@@ -5,8 +5,27 @@ import android.os.Bundle;
 
 public class Juego extends Activity {
     @Override
+    private VistaJuego vistaJuego;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.juego);
+        vistaJuego = (VistaJuego) findViewById(R.id.VistaJuego);
     }
+
+
+    @Override
+    protected void onDestroy() {
+        vistaJuego.getHilo().detener();
+        super.onDestroy();
+        @Override
+        protected void onPause() {
+            super.onPause();
+            vistaJuego.getHilo().pausar();
+        }
+        @Override
+        protected void onResume() {
+            super.onResume();
+            vistaJuego.getHilo().reanudar();
+        }
+
 }
