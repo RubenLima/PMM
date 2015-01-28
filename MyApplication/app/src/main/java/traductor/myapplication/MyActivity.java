bf95fd6c8db490b5333fef085bc67dd7f0e12a50
RubenLima <rubenperezperez304@gmail.com>
2015-01-28 10:51:06 +0100

    examen

---
+++ MyApplication/app/src/main/java/traductor/myapplication/MyActivity.java
@@ -0,0 +1,62 @@
+package traductor.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+public class MyActivity extends Activity {
+
+    EditText Etext;
+    Button boton;
+    public static int COD_RESPONSE =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        Etext = (EditText) findViewById(R.id.editText);
+       // miButton = (Button) findViewById(R.id.button);
+
+        boton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(MyActivity.this, sonido.class);
+                Bundle myBundle = new Bundle();
+               // Persona persona = personas.get(mySpinner.getSelectedItemPosition());
+               // myBundle.putSerializable("PROFILE", persona);
+                myIntent.putExtras(myBundle);
+                startActivityForResult(myIntent, COD_RESPONSE); // CREATED ProfileActivity in AndroidManifest.xml
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ MyApplication/app/src/main/java/traductor/myapplication/sonido.java
@@ -0,0 +1,57 @@
+package traductor.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * Created by mati on 28/01/15.
+ */
+public class sonido extends Activity {
+
+    public class Audio{
+
+        MediaPlayer mp;
+
+        public void playClick(Context context){
+            mp = MediaPlayer.create(context, R.raw.explosion);
+            mp.start();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sonido);
+        audio = new Audio();
+        audio.playClick(this);
+
+        Audio audio = new Audio(YourActivity.getApplicationContext());
+        audio.playClick();
+
+
+     //   MediaPlayer miMediaPlayer =
+        //        MediaPlayer.create(getContext(), R.raw.explosion);
+      //  miMediaPlayer.start();
+        final Button boton = (Button)findViewById(R.id.button);
+        Bundle act1Bundle = getIntent().getExtras();
+        boton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent retIntent = new Intent();
+                Bundle retBundle = new Bundle();
+                String retText = "Devuelto con éxito";
+                retBundle.putString("RETURNED", retText);
+                retIntent.putExtras(retBundle);
+                setResult(RESULT_OK, retIntent);
+                finish();
+            }
+        });
+    }
+}
